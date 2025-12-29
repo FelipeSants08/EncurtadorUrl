bdf4a078b4d78985e581e4bed3a466433356aee1
FelipeSants08 <felipe_santana.01@hotmail.com>
2025-12-29 15:56:27 -0300

    Adicionando JPA ao projeto e usando JPQL e outras soluções possíveis

---
+++ src/main/java/com/santana/encurtador/repository/UrlRepository.java
@@ -0,0 +1,31 @@
+package com.santana.encurtador.repository;
+
+import com.santana.encurtador.model.Url;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+public interface UrlRepository extends JpaRepository<Url, Long> {
+    Optional<Url> findByCodigoEncurtado(String urlEncurtada);
+
+    Optional<Url> findByCodigoEncurtadoAndDataUrlExpiradoAfter(String urlEncurtada, LocalDateTime data);
+
+    @Query("""
+        select u from Url u
+        where u.codigoEncurtado = :codigo
+            and  u.dataUrlExpirado > :agora
+    """)
+    Optional<Url> buscarAtivo(
+            @Param("codigo") String codigoEncurtado,
+            @Param("agora") LocalDateTime data
+    );
+
+    @Query("select u.url from Url u where u.codigoEncurtado = :codigo")
+    String urlOriginal(
+            @Param("codigo") String codigo
+    );
+
+}
